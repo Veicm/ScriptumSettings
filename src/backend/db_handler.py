9955c822f55638eaea27e2171f730757169e0200
Veicm <connormannes@gmx.de>
2025-08-06 17:24:09 +0200

    enhances folder structure and added a database handler

---
+++ src/backend/db_handler.py
@@ -0,0 +1,50 @@
+import sqlite3
+from handler_utils import clean_lection_list
+
+class DBHandler:
+    """
+    Handles all database interaction for the application.
+
+    Attributes:
+        None
+    """
+
+    def __init__(self) -> None:
+        pass
+
+    def _get_new_db_file(self, database_path: str) -> None:
+        """
+        Refreshes the database path so that the user can manage multiple databases.
+    
+        Args:
+            database_path (str): The path to your SQLite file.
+    
+        Returns:
+            None
+        """
+        self.connection = sqlite3.connect(database_path)
+        self.cursor = self.connection.cursor()
+
+    def get_tables(self) -> list[int]:
+        """
+        Searches the given database and returns all lection numbers.
+    
+        Args:
+            None
+    
+        Returns:
+            list[int]: The numbers of the founded lections.
+        """
+        self.cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+        raw: list[tuple[str]] = self.cursor.fetchall()
+        result: list[int] = clean_lection_list(raw)
+        return result
+        
+
+def main() -> None:
+    handler = DBHandler()
+    handler._get_new_db_file("data/demo.db")
+    handler.get_tables()
+
+if __name__ == "__main__":
+    main()
---
+++ src/backend/handler_utils.py
@@ -0,0 +1,22 @@
+import re
+
+def clean_lection_list(SQLite_output: list[tuple[str]]) -> list[int]:
+    """
+    Extracts the numbers of the lection from the raw output.
+
+    Args:
+        SQLite_output (list[tuple[str]]): The raw output from the SQLite3 function.
+
+    Returns:
+        list[int]: Only the number of the lections.
+    """
+    word_list: list[str] = []
+    number_list: list[int] = []
+    for entry in SQLite_output:
+        for word in entry:
+            word_list.append(word)
+
+    for entry in word_list:
+        number_list.append(int(re.search(r'\d+', entry).group()))
+
+    return number_list
